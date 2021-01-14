abe3f43b0de42baf5f434b0cb0d91ab672a434da
Bruno <brunomendesccb@gmail.com>
2021-01-14 12:08:18 -0300

    first tests

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+// Programa de teste. Feito na distro UBUNTU 20.04 LTS
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    cout << "Hello world!" << endl;
+
+    return 0;
+}
---
+++ variaveis.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int idade = 13;
+    cout << idade << endl;
+
+    return 0;
+}
